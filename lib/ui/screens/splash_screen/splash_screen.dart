4688cc64c5fb91ee0fc4864aacd4ab51fc2a8fcd
zsifat <zahidulislamsifat1@gmail.com>
2025-02-06 13:39:43 +0600

    Updated splash screen with new PNG image

--- lib/ui/screens/splash_screen/splash_screen.dart
+++ lib/ui/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,70 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xFF2B3760), // Background color similar to the image
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(),
+          Center(
+            child: ColorFiltered(
+              colorFilter: const ColorFilter.mode(
+                Colors.transparent,
+                BlendMode.multiply,
+              ),
+              child: Image.asset(
+                'assets/images/splash_image.jpg',
+                width: size.width * 0.8,
+                height: size.height * 0.5,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Text(
+            'ParentPal',
+            style: TextStyle(
+              fontSize: 66,
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Connect, Share, Care!',
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xFFF3FEFF),
+            ),
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Let's start!",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
