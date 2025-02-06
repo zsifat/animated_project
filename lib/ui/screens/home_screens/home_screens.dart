123b99e720fa665b9e1d8031f8abae5aeee30f1f
Md.Hemal <mdhemal202000@gmail.com>
2025-02-06 13:04:28 +0600

    pubspec updated, homescreen is created

---
+++ lib/ui/screens/home_screens/home_screens.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreens extends StatelessWidget {
+  const HomeScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: size.width,
+        ),
+      )
+    );
+  }
+}
