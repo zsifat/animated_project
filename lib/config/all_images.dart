1edc74fa6abb2d133d5676e8df69777236a1ab35
Md.Hemal <mdhemal202000@gmail.com>
2025-02-06 13:32:54 +0600

    all_images file added and updated

---
+++ lib/config/all_images.dart
@@ -0,0 +1,9 @@
+class AllImages{
+  //images
+
+  // icons
+  static String likeIcon = 'assets/icons/like_icon.svg';
+  static String messageIcon = 'assets/icons/message.svg';
+  static String shareIcon = 'assets/icons/share_icon.svg';
+  static String threeDotIcon = 'assets/icons/three_dot_icons.svg';
+}
