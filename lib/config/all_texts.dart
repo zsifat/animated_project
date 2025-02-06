423330e7c3c239dc54aa26c3bc2434959b2e9e8a
Md.Hemal <mdhemal202000@gmail.com>
2025-02-06 13:13:05 +0600

    all_texts file and home_screen updated

--- lib/config/all_texts.dart
+++ lib/config/all_texts.dart
@@ -1,3 +1,15 @@
 class AllTexts{
+  static String welcome = 'Welcome Back,';
+  static String userName = 'Wade Warren';
+  static String searchHint = 'What do you want to share?';
 
+  static String feed = 'Feed';
+  static String stories = 'Stories';
+  static String classes = 'Classes';
+  static String lifeHour = 'Life Hours';
+
+  static String likes = 'Likes';
+  static String comments = 'Comments';
+  static String share = 'Share';
+  
 }
