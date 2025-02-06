63c771cd5a7dbe1804fc7e15fa5d0af7058cbf11
Md.Hemal <mdhemal202000@gmail.com>
2025-02-06 12:51:22 +0600

    splash screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dribble_parentpal/ui/screens/splash_screen.dart';
+import 'package:dribble_parentpal/ui/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
